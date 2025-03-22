6b54e026999fca149bf1c908239c72bc3f9247f7
theerudito <erudito.tv@gmail.com>
2025-03-21 23:27:41 -0500

    v1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   server: {
     allowedHosts: ["react.between-bytes.tech", "localhost"],
     host: "0.0.0.0", // Esto expone el servidor en todas las interfaces de red
-    port: 5173, // O el puerto que desees usar
+    port: 300, // O el puerto que desees usar
   },
 });
