eb768bcba3f428f528a06557370898d3d55443a6
theerudito <erudito.tv@gmail.com>
2025-03-21 23:32:54 -0500

    v1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,4 +8,12 @@ export default defineConfig({
     host: "0.0.0.0", // Esto expone el servidor en todas las interfaces de red
     port: 3000, // O el puerto que desees usar
   },
+  build: {
+    outDir: "../backend/public",
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
 });
