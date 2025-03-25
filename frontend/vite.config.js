17f2cad8e9c4721c8a915b211fde5cce5db09ca9
theerudito <erudito.tv@gmail.com>
2025-03-24 20:41:08 -0500

    v1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,17 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: ["react.between-bytes.tech", "localhost"],
-    host: "0.0.0.0", // Esto expone el servidor en todas las interfaces de red
-    port: 3000, // O el puerto que desees usar
-  },
-  build: {
-    outDir: "../backend/public",
-  },
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
 });
